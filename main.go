e75357e78349e9cfbcf16d332bf08604a940f4f6
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-08-16 21:29:11 -0500

    feat: Add endpoint to save a string
    The `save-snippet` endpoint just saves the string that is POSTed to it.
    
    Also implemented the JavaScript functions that use this route.

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
+type Data struct {
+	Data string `json:"data"`
+}
+
 func main() {
+	data := Data{}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/index.html")
+	})
+
+	http.HandleFunc("/save-snippet", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := io.ReadAll(r.Body)
+		json.Unmarshal(body, &data)
 	})
 
 	fmt.Println("Server is running on port 8080")
